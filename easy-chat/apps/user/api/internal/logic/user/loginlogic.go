289369da8f7672682b6aba3594360582e498718d
BaihlUp <2677443264@qq.com>
2024-10-17 17:49:13 +0800

    easy-chat init

---
+++ easy-chat/apps/user/api/internal/logic/user/loginlogic.go
@@ -0,0 +1,44 @@
+package user
+
+import (
+	"context"
+	"easy-chat/apps/user/rpc/user"
+	"fmt"
+	"github.com/jinzhu/copier"
+
+	"easy-chat/apps/user/api/internal/svc"
+	"easy-chat/apps/user/api/internal/types"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type LoginLogic struct {
+	logx.Logger
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+}
+
+func NewLoginLogic(ctx context.Context, svcCtx *svc.ServiceContext) *LoginLogic {
+	return &LoginLogic{
+		Logger: logx.WithContext(ctx),
+		ctx:    ctx,
+		svcCtx: svcCtx,
+	}
+}
+
+func (l *LoginLogic) Login(req *types.LoginReq) (resp *types.LoginResp, err error) {
+	// todo: add your logic here and delete this line
+
+	loginResp, err := l.svcCtx.User.Login(l.ctx, &user.LoginReq{
+		Phone:    req.Phone,
+		Password: req.Password,
+	})
+	if err != nil {
+		return nil, err
+	}
+	fmt.Println("login")
+	var res types.LoginResp
+	copier.Copy(&res, loginResp)
+
+	return &res, nil
+}
